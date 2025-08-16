d5f8a737a879c99ac267ad3299faf104d5f4c025
prod42 <jb@prod42.net>
2025-08-16 14:37:01 -0400

    Refactor and simplify logging system; integrate with p42BaseLib.

--- Interfaces/IP42Logger.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace p42ConsoleLib.Interfaces;
-
-public interface IP42Logger
-{
-    void Log(string message);
-    void Debug(string message);
-    void Error(string message);
-    void Info(string message);
-}
---
+++ P42ConsoleLogger.cs
@@ -0,0 +1,54 @@
+﻿
+using p42BaseLib;
+using p42BaseLib.Interfaces;
+
+
+namespace p42ConsoleLib;
+
+
+public class P42ConsoleLogger() : P42Logger
+{
+    ConsoleColor infoColor = ConsoleColor.Green;
+    ConsoleColor errorColor = ConsoleColor.Red;
+    ConsoleColor debugColor = ConsoleColor.DarkGray;
+    ConsoleColor defaultColor = ConsoleColor.White;
+
+ 
+
+    static void WriteColoredLine(string text, ConsoleColor foreground, ConsoleColor? background = null)
+    {
+        var oldFg = Console.ForegroundColor;
+        var oldBg = Console.BackgroundColor;
+
+        Console.ForegroundColor = foreground;
+        if (background.HasValue) Console.BackgroundColor = background.Value;
+
+        Console.WriteLine(text);
+
+        Console.ForegroundColor = oldFg;
+        Console.BackgroundColor = oldBg;
+    }
+    
+
+    protected override void WriteEntry(p42BaseLib.LogType logType, string message)
+    {
+        ConsoleColor clr = defaultColor;
+        string _path = "";
+
+        clr = logType switch
+        {
+            LogType.Log => defaultColor,
+            LogType.Error => errorColor,
+            LogType.Debug => debugColor,
+            LogType.Info => infoColor,
+            _ => defaultColor,
+        };
+        
+
+
+        WriteColoredLine(message, clr);
+
+    }
+
+    
+}
